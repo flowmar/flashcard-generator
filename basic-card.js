d4e301d4bd07a45df7fd3854aa1694ff35468f44
flowmar <omar.imam@kineticvortex.com>
2017-05-21 23:46:47 -0500

    add basicCard constructor

---
+++ basic-card.js
@@ -0,0 +1,17 @@
+/*Rules: 1. define a node modle that exports a constructir
+for creating basic flashcards(module.exports = basicCard;)
+2. Constuctor needs 2 arguments, front and back
+3. constructed object needs a front property with the text
+for the front of the card(full)
+4. the construckted object also needs a back property with the back of the card text */
+
+function BasicCard(front, back) {
+    this.front = front;
+    this.back = back;
+}
+
+var card1 = new BasicCard("What is the capital of Maryland?", "Annapolis");
+var card2 = new BasicCard("What is the meaning of life", "Pizza, of course");
+
+console.log(card1);
+console.log(card2);
